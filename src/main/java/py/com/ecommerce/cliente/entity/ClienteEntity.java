17778e8f9d1f30b9dd7d3793377b72641a0598cc
MatiasWinter <matiaswinter014@gmail.com>
2025-11-04 00:23:56 -0300

    Agrega entity de cliente

---
+++ src/main/java/py/com/ecommerce/cliente/entity/ClienteEntity.java
@@ -0,0 +1,33 @@
+package py.com.ecommerce.cliente.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Table(name = "cliente")
+@Getter
+@Setter
+public class ClienteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long idCliente;
+
+    @Column(name = "nombre", length = 100, nullable = false)
+    private String nombre;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "telefono", length = 20)
+    private String telefono;
+
+    @Column(name = "direccion", columnDefinition = "TEXT")
+    private String direccion;
+
+    @Column(name = "estado", length = 20)
+    private String estado;
+}
